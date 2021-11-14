e6aad1ad14d71d495f8980340793a207f2993f50
abbycarr <carr.ab@husky.neu.edu>
2021-11-13 19:55:37 -0500

    Initial Commit

---
+++ MindfulMagic/MindfulMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulMagicApp.swift
+//  MindfulMagic
+//
+//  Created by Abby Carr on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MindfulMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
